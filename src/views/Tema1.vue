6c07d35662d7aa053de1fa56fc9f53a6456c1ff3
SANDRATRIANA <sandrajem23@hotmail.com>
2022-04-11 23:13:34 -0500

    avance 2

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -47,6 +47,119 @@
       .col-lg-5.p-0(data-aos="fade-left")
         img(src='@/assets/curso/tema1/img04.png')
 
+    .row.justify-content-center
+    .col-md-10.col-lg-4.mb-4.mb-lg-0
+      .TFcontainer
+        img.TFimage(src='@/assets/curso/tema1/img07.png', alt='Fase 1')
+        .TFoverlay--01.text-black.p-3
+          h5 Confidencialidad:
+          p.mb-0 La información sólo puede ser accedida por la persona a la que está destinada y ninguna otra persona puede acceder a ella.
+
+    .row.align-items-center.mb-5
+      .col-lg-2.col-md-6.col-6(data-aos="fade-right").mb-5
+        img(src='@/assets/curso/tema1/img12.png')  
+    .row.justify-content-center
+      p Se conoce como cifrado:
+      .col-lg-8  
+        .titulo-sexto.color-acento-contenido(data-aos="fade-right")
+          h5 Figura 2
+          p.mb-0 Cifrado
+        figure.mb-5(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img13.svg')  
+      p A continuación, se presenta un breve tratamiento de los cifrados, para un tratamiento completo.
+    .BGIMG02.mb-5 
+      SlyderA(tipo="b")(data-aos="fade-right")
+        .row.justify-content-center.align-items-center.p-5
+          .col-lg-6
+            img(src='@/assets/curso/tema1/img16.png')
+          .col-lg-5
+            p <b>La transposición y la sustitución</b> se realizan en cascada; por ejemplo, en un sistema de este tipo, denominado sistema de fraccionamiento, la sustitución se realiza primero a partir de símbolos en texto plano a múltiples símbolos en el texto cifrado, que luego se superpone a una transposición.
+            p Todas las operaciones o etapas de la transformación de un mensaje se efectúan de conformidad con una regla definida por una clave secreta que solo conocen el emisor del mensaje y el receptor previsto.
+            .cajon.color-acento-contenido.p-5
+             h4 TODAS LAS CIFRAS 
+              p Implican una transposición o sustitución, o una combinación de estas dos operaciones matemáticas, es decir, cifras de productos. 
+        .row.justify-content-center.align-items-center.p-5
+          .col-lg-6
+            img(src='@/assets/curso/tema1/img17.png')
+          .col-lg-6
+            .cajon.color-acento-contenido.p-5
+              h4 EN LOS SISTEMAS POR TRANSPOSICIÓN
+              p Los elementos del texto simple (por ejemplo, una letra, una palabra o una cadena de símbolos) se reordenan sin que cambie la identidad de los elementos, 
+        .row.justify-content-center.align-items-center.p-5
+          .col-lg-6
+            img(src='@/assets/curso/tema1/img18.png')
+          .col-lg-6
+            .cajon.color-acento-contenido.p-5
+              h4 EN LOS SISTEMAS POR SUSTITUCIÓN
+              p Esos elementos se sustituyen por otros objetos o grupos de objetos sin que se modifique su secuencia, en los sistemas de cifrado de productos.
+        
+    .row.mb-5.justify-content-center
+      p Se conoce como cifrado:
+      .col-lg-8  
+        .titulo-sexto.color-acento-contenido(data-aos="fade-right")
+          h5 Figura 3
+          p.mb-0 Cifrado
+        figure.mb-5(data-aos="fade-right")
+          img(src='@/assets/curso/tema1/img19.svg') 
+
+    .row.align-items-center.mb-5
+      .col-lg-7(data-aos="fade-right")
+        p.mb-3 Se han utilizado comúnmente dispositivos o máquinas de cifrado para cifrar y descifrar mensajes. El primer dispositivo de cifrado parece haber sido empleado por los antiguos griegos alrededor del año 400 a.C. para las comunicaciones secretas entre los comandantes militares. Este dispositivo, llamado la guadaña, consistía en un bastón cónico alrededor del cual se envolvía en espiral un trozo de pergamino inscrito con el mensaje, cuando se desenvolvía, el pergamino llevaba un incomprensible juego de letras, pero cuando se envolvía alrededor de otro bastón de idénticas proporciones, el texto original reaparecía. 
+        p.mb-0 Otros dispositivos simples conocidos como discos cifrados fueron utilizados por los gobiernos europeos para las comunicaciones diplomáticas a finales de 1400. Estos dispositivos consistían en dos círculos concéntricos giratorios, ambos con una secuencia de 26 letras; un disco se usaba para seleccionar las letras de texto plano, mientras que el otro se usaba para el componente de cifrado correspondiente.
+      .col-lg-5.p-0(data-aos="fade-left")
+        img(src='@/assets/curso/tema1/img20.png') 
+    
+    separador
+
+    #t_1_2.titulo-segundo.color-acento-contenido(data-aos="fade-right")
+      h2 1.2	Tipos y cifrados
+    p A continuación, estudiará los tipos de criptografía.
+
+    .BGIMG03.mb-5 
+      SlyderA(tipo="b")(data-aos="fade-right")
+        .row.justify-content-center.align-items-center.p-5
+          .col-lg-5
+            h3 Criptografía de clave simétrica
+            p Es un sistema de encriptación en el que el emisor y el receptor de un mensaje utilizan una única clave común para encriptar y desencriptar los mensajes, los sistemas de clave simétrica son más rápidos y simples, pero el problema es que el emisor y el receptor tienen que intercambiar la clave de alguna manera segura. El sistema de criptografía de clave simétrica más popular es el Sistema de Cifrado de Datos (DES).
+          .col-lg-6
+            img(src='@/assets/curso/tema1/img21.svg')
+        .row.justify-content-center.align-items-center.p-5
+          .col-lg-5
+            h3 Funciones hash
+            p No hay uso de ninguna clave en este algoritmo. Un valor hash con longitud fija se calcula según el texto plano, lo que hace imposible que el contenido del texto plano se recupere. Muchos sistemas operativos utilizan funciones hash para encriptar las contraseñas.
+          .col-lg-6
+            img(src='@/assets/curso/tema1/img22.svg') 
+        .row.justify-content-center.align-items-center.p-5
+          .col-lg-5
+            h3 Criptografía de clave asimétrica
+            p En este sistema, se utiliza un par de claves para encriptar y desencriptar información; se utiliza una clave pública para el cifrado y una clave privada para el descifrado; la clave pública y la privada son diferentes, incluso si la clave pública es conocida por todos, el receptor previsto solo puede descifrarla porque solo él conoce la clave privada.
+          .col-lg-6
+            img(src='@/assets/curso/tema1/img23.svg')
+    .col-lg-12.mb-5      
+      p Ahora aborde algunos estándares y algoritmos:
+    
+    AcordionA.mb-5(tipo="b" clase-tarjeta="tarjeta tarjeta--azul")(data-aos="fade-right")
+      .row(titulo="Estándar de cifrado avanzado (AES)")
+        .col-md-8.col-11.mb-4.mb-md-0
+          p El cifrado AES fue establecido por el Instituto Nacional de Estándares y Tecnología de los Estados Unidos (NIST) en 2001 y su objetivo es ofrecer una especificación para la encriptación de datos electrónicos.
+
+          p Los desarrolladores del AES son dos criptógrafos belgas: Joan Deamen y Vincent Rijmen; se sabe que el dúo ha propuesto su cifrado en bloque al Instituto Nacional de Estándares y Tecnología durante el proceso de selección de un AES. Para seleccionar un AES seguro, el NIST consideró tres diferentes bloques de cifrado de la familia de cifrado Rijndael. Estos tres cifrados seleccionados eran todos de 128 bits, pero las longitudes de sus claves eran de 128, 192 y 265 bits.
+          p Al final, se seleccionó el cifrado en bloque desarrollado por Rijmen y Daemen. Este AES fue adoptado por primera vez en los Estados Unidos, pero con el tiempo se convirtió en la corriente principal en todo el mundo; el AES se caracteriza por ser un cifrado en bloque simétrico, en otras palabras, utiliza la misma clave para el cifrado y el descifrado. 
+        .col-md-9.col-6
+          figure
+            img(src='@/assets/curso/tema1/img25.svg')
+      .row(titulo="Estándar de encriptación de datos (DES)")
+        .col-md-8.col-11.mb-4.mb-md-0
+          p DES es el nombre del estándar FIPS (Federal Information Processing Standard) 46-3, el cual describe el algoritmo de cifrado de datos (DEA), que es un sistema criptográfico simétrico originalmente diseñado para implementación en el hardware; también es usado para encriptación de usuario simple para almacenar datos en el disco de forma encriptada, un algoritmo de cifrado en bloque que toma texto plano en bloques de 64 bits y los convierte en texto cifrado utilizando claves de 48 bits.
+        .col-md-3.col-6
+          figure
+            img(src='@/assets/curso/tema1/img26.png')
+      .row(titulo="Algoritmos RC4, RC5, RC6")
+        .col-md-8.col-11.mb-4.mb-md-0
+          p DES es el nombre del estándar FIPS (Federal Information Processing Standard) 46-3, el cual describe el algoritmo de cifrado de datos (DEA), que es un sistema criptográfico simétrico originalmente diseñado para implementación en el hardware; también es usado para encriptación de usuario simple para almacenar datos en el disco de forma encriptada, un algoritmo de cifrado en bloque que toma texto plano en bloques de 64 bits y los convierte en texto cifrado utilizando claves de 48 bits.
+        .col-md-3.col-6
+          figure
+            img(src='@/assets/curso/tema1/img27.png')
 </template>
 
 <script>
